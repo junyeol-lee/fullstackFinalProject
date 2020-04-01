c5fc0971a35332b92cde85bdbd16eddaa0938923
Junyeol_Lee <alltheway544@gmail.com>
2020-04-01 18:24:54 -0500

    Category model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_232245) do
+ActiveRecord::Schema.define(version: 2020_04_01_232424) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "gsts", force: :cascade do |t|
     t.decimal "rate"
