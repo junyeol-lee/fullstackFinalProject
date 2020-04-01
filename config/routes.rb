aca2e19fac18f335ae5ac5d01b8e71b10e8f6c4e
Junyeol_Lee <alltheway544@gmail.com>
2020-04-01 18:32:01 -0500

    products controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'products/show'
+  get 'products/index'
   get 'categories/index'
   get 'categories/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
