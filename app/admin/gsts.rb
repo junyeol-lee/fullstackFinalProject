aa06b32f34e53160209231e09133bb6ab4fd20f7
Junyeol_Lee <alltheway544@gmail.com>
2020-04-01 19:11:51 -0500

    added resources to the active admin

---
+++ app/admin/gsts.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Gst do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :rate
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:rate]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
---
+++ app/admin/products.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Product do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :category_id, :name, :description, :cost_cents, :product_status
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:category_id, :name, :description, :cost_cents, :product_status]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
