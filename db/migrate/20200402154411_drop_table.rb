087b72f47e473dbc8cd3c89c735455b1c712946c
Junyeol_Lee <alltheway544@gmail.com>
2020-04-02 10:50:00 -0500

    removed customer table

---
+++ db/migrate/20200402154411_drop_table.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class DropTable < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :orders, :customer_id, :references
+    drop_table(:customers)
+  end
+end
