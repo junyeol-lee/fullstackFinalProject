b41b4dac2d15d0999625a2426ae3a0cb95d9ce40
Junyeol_Lee <alltheway544@gmail.com>
2020-04-05 15:36:36 -0500

    session is done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,21 +1,40 @@
 # frozen_string_literal: true
 
 class ProductsController < ApplicationController
+  before_action :initialize_session
+  before_action :load_cart
+
   def index
     @products = if params['category_id']
-                  Product.where(category_id: params['category_id'])
+                  Product.includes(:category).where(category_id: params['category_id'])
                 else
-                  Product.all
-    end
+                  Product.includes(:category).all
+                end
   end
 
   def show
     @product = Product.includes(:category).find(params['id'])
   end
 
-  # Get /search/?search_term=user+search+terms
-  def search
-    # DANGER DANGER
-    @products = Product.where('name LIKE ?', "%#{params[:search_term]}%")
+  def add_to_cart
+    id = params['id'].to_i
+    session[:cart] << id unless session[:cart].include?(id)
+    redirect_to products_url
   end
+
+  def remove_from_cart
+    id = params['id'].to_i
+    session[:cart].delete(id)
+    redirect_to products_url
+  end
+end
+
+private
+
+def initialize_session
+  session[:cart] ||= []
+end
+
+def load_cart
+  @cart = Product.find(session[:cart])
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources 'categories', only: %i[index show]
   get 'search', to: 'products#search', as: 'search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  post 'products/add_to_cart/:id', to: 'products#add_to_cart', as: 'add_to_cart'
+  delete 'products/remove_from_cart/:id', to: 'products#remove_from_cart', as: 'remove_from_cart'
   root to: 'pages#home'
 end
